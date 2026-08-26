8d7da7ed30d112d37499939358021b2f686d02f7
Fabio Cirilo <cirilofabio84@gmail.com>
2026-08-26 12:51:27 +0100

    Creating a registration page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,37 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Platform } from 'react-native'
+import {
+  AreaInput,
+  Background,
+  Container,
+  Input,
+  Logo,
+  SubmitButton,
+  SubmitText
+} from '../SignIn/styles'
 
 export default function () {
   return (
-    <View>
-      <Text>Tela de Cadastro</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <Logo source={require('../../assets/Logo.png')} />
+
+        <AreaInput>
+          <Input placeholder="Nome" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Senha" />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Cadastrar</SubmitText>
+        </SubmitButton>
+      </Container>
+    </Background>
   )
 }
