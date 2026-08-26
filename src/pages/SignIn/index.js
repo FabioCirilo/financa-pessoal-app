496dd97c67acce97f3172c082e14c093ca17d042
Fabio Cirilo <cirilofabio84@gmail.com>
2026-08-26 12:27:37 +0100

    Creating a login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Platform, Text, View } from 'react-native'
+import {
+  AreaInput,
+  Background,
+  Container,
+  Input,
+  Link,
+  LinkText,
+  Logo,
+  SubmitButton,
+  SubmitText
+} from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 export default function SignIn() {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>Tela Login</Text>
-    </View>
+    <Background>
+      <Container behavior={Platform.OS === 'ios' ? 'padding' : ''} enabled>
+        <Logo source={require('../../assets/Logo.png')} />
+
+        <AreaInput>
+          <Input placeholder="Email" />
+        </AreaInput>
+        <AreaInput>
+          <Input placeholder="Senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Entrar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar uma conta!</LinkText>
+        </Link>
+      </Container>
+    </Background>
   )
 }
