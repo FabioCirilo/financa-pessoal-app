1b7c10e3428cb33bdf2c15176b6b53ac3cae4e15
Fabio Cirilo <cirilofabio84@gmail.com>
2026-08-27 09:35:59 +0100

    Loading during registration

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Platform } from 'react-native'
+import { ActivityIndicator, Platform } from 'react-native'
 import {
   AreaInput,
   Background,
@@ -12,12 +12,14 @@ import {
 import { AuthContext } from '../../contexts/auth'
 
 export default function SignUp() {
-  const { signUp } = useContext(AuthContext)
+  const { signUp, loadingAuth } = useContext(AuthContext)
   const [nome, setNome] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   function handleSignUp() {
+    if (nome === '' || email === '' || password === '') return
+
     signUp(email, password, nome)
   }
 
@@ -52,7 +54,11 @@ export default function SignUp() {
         </AreaInput>
 
         <SubmitButton onPress={handleSignUp}>
-          <SubmitText>Cadastrar</SubmitText>
+          {loadingAuth ? (
+            <ActivityIndicator size={20} color="#fff" />
+          ) : (
+            <SubmitText>Cadastrar</SubmitText>
+          )}
         </SubmitButton>
       </Container>
     </Background>
