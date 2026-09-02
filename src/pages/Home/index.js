2d1172f4d677b170cacd6b079deee2eb97bddd75
Fabio Cirilo <cirilofabio84@gmail.com>
2026-09-02 09:43:53 +0100

    Creating logout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import { create } from 'axios'
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, View, Button } from 'react-native'
+
+import { AuthContext } from '../../contexts/auth'
 
 export default function HomePage() {
+  const { signOut, user } = useContext(AuthContext)
+
   return (
     <View>
       <Text>Pagina Home</Text>
+      <Text>Nome: {user.name}</Text>
+      <Button title="Sair" onPress={() => signOut()} />
     </View>
   )
 }
