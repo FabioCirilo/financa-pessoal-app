e66fe99614e5d24d7897d96ca1a0cd23080e658b
Fabio Cirilo <cirilofabio84@gmail.com>
2026-09-03 16:51:40 +0100

    Recording expenses

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,13 +1,57 @@
 import React, { useState } from 'react'
 import { Background, Input, SubmitButton, SubmitText } from './styles'
 import Header from '../../components/Header'
-import { Keyboard, TouchableWithoutFeedback, View } from 'react-native'
+import { Alert, Keyboard, TouchableWithoutFeedback, View } from 'react-native'
 import RegisterTypes from '../../components/RegisterTypes'
+import api from '../../services/api'
+import { format } from 'date-fns'
+import { useNavigation } from '@react-navigation/native'
 
 export default function NewMovement() {
+  const navigation = useNavigation()
+
   const [labelInput, setLabelInput] = useState('')
   const [valueInput, setValueInput] = useState('')
   const [type, setType] = useState('receita')
+
+  function handleSubmit() {
+    Keyboard.dismiss()
+
+    if (isNaN(parseFloat(valueInput)) || type === null) {
+      alert('Preencha todos os campos')
+      return
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Tipo: ${type} - Valor: ${parseFloat(valueInput)} - Descrição: ${labelInput}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handlleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handlleAdd() {
+    Keyboard.dismiss()
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput('')
+    setValueInput('')
+    navigation.navigate('Home', { reload: true })
+  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -29,7 +73,7 @@ export default function NewMovement() {
 
           <RegisterTypes type={type} sendTypeChanged={item => setType(item)} />
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </View>
--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: 'https://localhost:3333'
-  //192.168.0.132
-  baseURL: 'http://192.168.1.212:3333' // Usa-se o IP da maquina quando o servidor esta rodar localmente
+  //192.168.0.132 / 192.168.1.212:3333
+  baseURL: 'http://192.168.0.132:3333' // Usa-se o IP da maquina quando o servidor esta rodar localmente
 })
 
 export default api
