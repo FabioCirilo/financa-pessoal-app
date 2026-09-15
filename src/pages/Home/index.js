e63ddf34be857bab0d1e32709d699f25e30ebc07
Fabio Cirilo <cirilofabio84@gmail.com>
2026-09-15 18:31:37 +0100

    Adjusting date formatting

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,7 +24,10 @@ export default function HomePage() {
     let isActive = true
 
     async function getMovements() {
-      let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+      //let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+      let date = new Date(dateMovements)
+      let onlyDate = date.valueOf() + date.getTimezoneOffset() * 60 * 1000
+      let dateFormated = format(onlyDate, 'dd/MM/yyyy')
 
       const receives = await api.get('/receives', {
         params: {
